e6d1390cbef5531d13db4843dc1a1ca5c34eb61e
imalexlov1 <alex.lovin.pro@gmail.com>
2025-12-16 17:12:24 +0100

    ajout du script pour le deplacement de la raquette

--- td-1-godot/raquette/Control Joueur.gd
+++ td-1-godot/raquette/Control Joueur.gd
@@ -1,11 +1,19 @@
-extends Node
+extends Node2D
 
+@export var speed : int = 400 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
+func _physics_process(delta: float) -> void:
+	var velocity = Vector2.ZERO # Start with no movement
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("p1_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("p1_left"):
+		velocity.x -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+		position += velocity * delta
