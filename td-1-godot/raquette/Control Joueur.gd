26fe9c5eb7cafad1f21a6f03fa76ca989ac6e863
imalexlov1 <alex.lovin.pro@gmail.com>
2025-12-19 09:57:52 +0100

    clear le projet des labels inutile

--- td-1-godot/raquette/Control Joueur.gd
+++ td-1-godot/raquette/Control Joueur.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 @export var speed : int = 400 
+var screen_size : Vector2 # Pour stocker la taille de l'écran
 
 func _ready() -> void:
-	pass 
+	# On récupère la taille de la fenêtre au démarrage du jeu
+	screen_size = get_viewport_rect().size
 
 func _physics_process(delta: float) -> void:
-	var velocity = Vector2.ZERO # Start with no movement
+	var velocity = Vector2.ZERO 
 
 	if Input.is_action_pressed("p1_right"):
 		velocity.x += 1
@@ -15,5 +17,5 @@ func _physics_process(delta: float) -> void:
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		
 		position += velocity * delta
+		position.x = clamp(position.x, 0, screen_size.x)
