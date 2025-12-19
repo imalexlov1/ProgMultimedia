297b4cf6fbecb2a40f4b8d7197e57a24697ffa8a
Cracotte <julesgp31@gmail.com>
2025-12-19 11:47:22 +0100

    attempted adding physics to ball behavior

---
+++ td-1-godot/Ball/ball.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+@export var speed := 450.0
+var velocity: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	velocity = Vector2(1, -1).normalized() * speed
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
